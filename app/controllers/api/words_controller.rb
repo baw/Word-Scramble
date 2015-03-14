c8016c35c95812c0b7f64655d69fa21e34b302de
Brian Weiser <iamamthwiz+GitHub@gmail.com>
2015-03-14 11:56:13 -0700

    add random word api endpoint

---
+++ app/controllers/api/words_controller.rb
@@ -0,0 +1,17 @@
+class Api::WordsController < ApplicationController
+  def randomWord
+    min = params[:min].to_i
+    max = params[:max].to_i
+    
+    word = Word.randomWord(min, max)
+    if word.nil?
+      render status: :unprocessable_entity, json: {
+        error: "Unprocessable request"
+      }
+    else
+      render json: {
+        word: word.text
+      }
+    end
+  end
+end
